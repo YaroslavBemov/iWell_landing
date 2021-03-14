6250924dc90f423462c0b0903aef6018d59409ce
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-03-14 21:59:43 +0300

    add fyway slider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import 'normalize.css'
 import './sass/main.sass'
 // import './sass/modal.sass'
 
+import './js/careSlides'
 import modal from './js/modal'
+import Swiper from 'swiper/bundle'
+import 'swiper/swiper-bundle.css'
 
 // const $ = {}
 // window.$ = {}
 // $.modal = modal
 
 // window.m = modal()
+
+const swiper = new Swiper('.swiper-container.fyway__swiper-container')
